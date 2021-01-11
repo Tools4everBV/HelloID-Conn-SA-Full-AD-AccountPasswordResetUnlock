e6ae9f5753e06fa8043804a82e580dc3532544f5
Michiel85 <68066270+Michiel85@users.noreply.github.com>
2021-01-11 13:48:41 +0100

    Updated file with Powershell data source

---
+++ Manual resources/[task]_AD-user-reset-unlock.ps1
@@ -0,0 +1,30 @@
+try {
+    $adUser = Get-ADuser -Filter { UserPrincipalName -eq $userPrincipalName }
+    HID-Write-Status -Message "Found AD user [$userPrincipalName]" -Event Information
+    HID-Write-Summary -Message "Found AD user [$userPrincipalName]" -Event Information
+} catch {
+    HID-Write-Status -Message "Could not find AD user [$userPrincipalName]. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Failed to find AD user [$userPrincipalName]" -Event Failed
+}
+
+if($blnreset -eq 'true'){
+    try {
+    	Set-ADAccountPassword -Identity $adUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+    	HID-Write-Status -Message "Password reset: $userPrincipalName" -Event Success
+    	HID-Write-Summary -Message "Password reset: $userPrincipalName" -Event Success
+    } catch {
+        HID-Write-Status -Message "Could not reset password [$userPrincipalName]. Error: $($_.Exception.Message)" -Event Error
+        HID-Write-Summary -Message "Failed to reset pasword [$userPrincipalName]" -Event Failed
+    }
+}
+
+if($blnunlock -eq 'true'){
+    try {
+	    Unlock-ADAccount -Identity $adUser
+	    HID-Write-Status -Message "Unlock account: $userPrincipalName" -Event Success
+	    HID-Write-Summary -Message "Unlock account: $userPrincipalName" -Event Success
+    } catch {
+        HID-Write-Status -Message "Could not unlock [$userPrincipalName]. Error: $($_.Exception.Message)" -Event Error
+        HID-Write-Summary -Message "Failed to unlock [$userPrincipalName]" -Event Failed
+    }
+}
